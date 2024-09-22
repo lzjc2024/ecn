a40103aa8631205c1ec22d21718cdc074a5baf1b
lzjc2024 <lzjc1613@outlook.com>
2024-09-22 08:36:27 +0800

    'url.js'

--- url.js
+++ url.js
@@ -1,6 +1,3 @@
 var url = 'https://7982a20b.r26.cpolar.top';
-<<<<<<< HEAD
 var lastTime = 'Sat Sep 21 2024 23:56:56 GMT+0800 (中国标准时间)';
-=======
 var lastTime = 'Sun Sep 22 2024 08:25:18 GMT+0800 (中国标准时间)';
->>>>>>> e488587 ('url.js')
